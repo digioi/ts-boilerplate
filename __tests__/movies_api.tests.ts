6072f1ca9a3b4883a83c3ca82822fa27f052d53a
Mike DiGioia <mdigioia@aps.org>
2019-03-23 18:35:10 -0400

    Updated the tests

--- __tests__/movies_api.tests.ts
+++ __tests__/movies_api.tests.ts
@@ -3,16 +3,14 @@ import "axios"
 
 it("should fetch movie", async () => {
   const { data } = await getMovie("tt0076759");
+  expect.assertions(2);
   expect(data.imdbID).toEqual("tt0076759")
   expect(data.Title).toEqual('Star Wars: Episode IV - A New Hope')
 });
 
-it("if invalid should throw error and catchd", async () => {
-  try {
-    await getMovie("12341");
-    expect(true).toEqual(false)
-  } catch (error) {
-    expect(true).toEqual(true)
-  }
-  
+it("if invalid should throw error and catchd", () => {
+  expect.assertions(1);
+  expect(getMovie("12341")).rejects.toEqual({
+    "status": 404
+  });
 });
